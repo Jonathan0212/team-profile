fd4b0448b95ca0733d2ed6811c78d17eeed528a1
Your Name <Vaaax@Jon-PC>
2022-02-17 23:12:52 -0600

    added test to managertest js

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -1,7 +1,20 @@
-const Engineer = require('../lib/Manager');
+const manager = require('../lib/Manager');
+const employee = require('../lib/Employee');
 
-jest.mock('../lib/Manager.js');
+test('Can set office number via constructor arguement', () => {
+    const testValue = 100;
+    const m = new manager('David', 1, 'david@test.com', testValue);
+    expect(m.officeNumber).toBe(testValue);
+});
 
-test ('creates a Engineer profile', () => {
+test('getRole() should return \"Manager\"', () => {
+    const testValue = 'Manager';
+    const m = new manager('Frank', 1, 'frank@test.com', 100);
+    expect(m.getRole()).toBe(testValue);
+});
 
-})
+test('Can get office number via getOffice()', () => {
+    const testValue = 100;
+    const m = new manager('Amy', 1, 'amy@test.com', testValue);
+    expect(m.getOfficeNumber()).toBe(testValue);
+});
