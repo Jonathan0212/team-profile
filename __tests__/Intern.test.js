ce9bd0abf9743782b2ad129a96a5d805f35a30d0
Your Name <Vaaax@Jon-PC>
2022-02-17 19:00:03 -0600

    added test to interntest js

--- __tests__/Intern.test.js
+++ __tests__/Intern.test.js
@@ -1,7 +1,19 @@
-const Engineer = require('../lib/Intern');
+const Intern = require('../lib/Intern');
 
-jest.mock('../lib/Intern.js');
+test('Can get school via construction', () => {
+    const testValue = "Harvard University";
+    const i = new Intern('Joey', 1, 'Joey@test.com', testValue );
+    expect(i.school).toBe(testValue);
+});
 
-test ('creates a Engineer profile', () => {
+test('getRole(), should return \"Intern\"', () => {
+    const testValue = 'Intern';
+    const i = new Intern ('Joe', 1, 'Joe@test.com', 'Harvard University');
+    expect(i.getRole()).toBe(testValue);
+});
 
-})
+test('Can get school via getSchool()', () => {
+    const testValue = 'Harvard University';
+    const i = new Intern ('Jimmy', 1, 'Jimmy@test.com', testValue);
+    expect(i.getSchool()).toBe(testValue);
+});
