1e92316c7178eb2adaa5e0b04f20fa2aeed4c113
Your Name <Vaaax@Jon-PC>
2022-02-17 23:29:58 -0600

    updated half of js files and working on index

--- index.js
+++ index.js
@@ -1,21 +1,9 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
-const inqurier = require("require");
+const inqurier = require("inquirer");
+const fs = require('fs');
+const path = require('path')
+const generateTeam = require('./src/pagetemplate');
 const teamMembers = [];
 
-function createTeam() {
-
-}
-
-function createIntern() {
-
-}
-
-function createEngineer() {
-
-}
-
-function endTeamCreation() {
-
-}
