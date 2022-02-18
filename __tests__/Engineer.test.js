4c18b99617e39a0168f9f528296058369e70b85c
Your Name <Vaaax@Jon-PC>
2022-02-17 18:16:21 -0600

    added test to engineertest js

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -1,7 +1,21 @@
 const Engineer = require('../lib/Engineer');
 
-jest.mock('../lib/Engineer.js');
+test("Can set GitHub account via constructor", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer('Joey', 1, 'Joey@test.com', testValue);
+    expect(e.github).toBe(testValue);
+});
 
-test ('creates a Engineer profile', () => {
+test ("getRole() should return \"Engineer\"", () => {
+    const testValue = "Engineer";
+    const e = new Engineer('Johnny', 1, 'Johnny@test.com', 'GithubUser')
+    expect(e.getRole()).toBe(testValue);
+});
 
-})
+
+test ("Can get Github username via getGithub()", () => {
+    const testValue = "GitHubUser";
+    const e = new Engineer('Steve', 1, 'Steve@test.com', 'testValue')
+    expect(e.getGithub()).toBe(testValue);
+});
+
