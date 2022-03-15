0bb9cd977f454c272c6e187e06a651d2c126a2d9
Your Name <Vaaax@Jon-PC>
2022-03-14 21:06:39 -0500

    finished folders in lib

--- index.js
+++ index.js
@@ -7,3 +7,59 @@ const path = require('path')
 const generateTeam = require('./src/pagetemplate');
 const teamMembers = [];
 
+function appMenu() {
+    console.log('Creating Manager');
+    inqurier.prompt([
+        {
+            type: 'input',
+            name: 'managerName',
+            messagee: "What is your manager's name?",
+            validate: function (value) {
+                if (value === ''){
+                    return 'Please enter a valid name';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerId',
+            message: "What is your manager's ID?",
+            validate: function (value){
+                if (value === ''){
+                    return 'Please enter a valid ID';
+                }
+                return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerEmail',
+            message: "What is your manager's email?",
+            validate: function (value){
+            if (value ===''){
+                return 'Please enter a valid email!';
+            }
+            return true;
+            }
+        },
+        {
+            type: 'input',
+            name: 'managerOfficeNumber',
+            message: "What is your manager's office number?",
+            validate: function (value){
+            if (value ===''){
+                return 'Please enter a office number';
+            }
+            return true;
+            }
+        },
+    ])
+
+    .then((answers) => {
+        const manager = new Manager(answers.managerName, answers.managerId, answers.managerEmail, answers.managerOfficeNumber);
+        teamMembers.push(manager);
+        console.log(teamMembers);
+        createTeam();
+    })
+} 
